26d1e0e792a6331bb468fd7434425e6f7792c184
Newton Duarte Cavalcanti <newton_@hotmail.com>
2021-07-18 18:18:25 -0300

    feat: load title texts based on current language by xml file

--- Assets/Scripts/Commands.cs
+++ Assets/Scripts/Commands.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Commands : MonoBehaviour
 {
     FadeController _fadeController;
     OptionsController _optionsController;
 
+    LoadXMLFile _loadXMLFile;
+
+    [SerializeField] Text txtOptions;
+    [SerializeField] Text txtStart;
+
     private void Start()
     {
         _fadeController = FindObjectOfType(typeof(FadeController)) as FadeController;
         _optionsController = FindObjectOfType(typeof(OptionsController)) as OptionsController;
+        _loadXMLFile = FindObjectOfType(typeof(LoadXMLFile)) as LoadXMLFile;
+
+        loadTitleTexts();
     }
 
     private void Update()
@@ -25,5 +34,20 @@ public class Commands : MonoBehaviour
     {
         _optionsController.StartCoroutine(_optionsController.changeMusic(_optionsController.startClip));
         _fadeController.startFade(2);
+    }
+
+    void loadTitleTexts()
+    {
+        txtOptions.text = _loadXMLFile.titleInterface[0];
+        txtStart.text = _loadXMLFile.titleInterface[1];
+    }
+
+    public void changeLanguage(string language)
+    {
+        PlayerPrefs.SetString("language", language);
+
+        _loadXMLFile.loadXMLData();
+
+        loadTitleTexts();
     }
 }
