9c17c4c9b60acef0bf306bac69388b8e1d871daa
Newton Duarte Cavalcanti <newton_@hotmail.com>
2021-08-02 14:30:04 -0300

    feat: add get next stage description index function

--- Assets/Scripts/GlobalVariables.cs
+++ Assets/Scripts/GlobalVariables.cs
@@ -1,4 +1,21 @@
 public static class GlobalVariables
 {
     public static int nextStage = 0;
+
+    public static int getNextStageDescriptionIndex()
+    {
+        switch (nextStage)
+        {
+            case 3:
+                return 2;
+            case 4:
+                return 0;
+            case 5:
+                return 3;
+            case 6:
+                return 1;
+            default:
+                return 1;
+        }
+    }
 }
